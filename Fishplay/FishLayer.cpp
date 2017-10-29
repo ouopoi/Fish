f3153fc1d2d6fa8c1773a6b53f080a5ee52d7750
ouopoi <32032125+ouopoi@users.noreply.github.com>
2017-10-29 12:06:01 +0800

    Add files via upload

---
+++ Fishplay/FishLayer.cpp
@@ -0,0 +1,15 @@
+#include "FishLayer.h"
+FishLayer::FishLayer()
+{
+}
+FishLayer::~FishLayer()
+{
+}
+bool FishLayer::init()
+{
+	if (!cocos2d::Layer::init())
+	{
+		return false;
+	}
+	return true;
+}
---
+++ Fishplay/MenuLayer.cpp
@@ -0,0 +1,15 @@
+#include "MenuLayer.h"
+MenuLayer::MenuLayer()
+{
+}
+MenuLayer::~MenuLayer()
+{
+}
+bool MenuLayer::init()
+{
+	if (!cocos2d::Layer::init())
+	{
+		return false;
+	}
+	return true;
+}
